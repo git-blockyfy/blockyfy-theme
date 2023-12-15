e4ba590e7006a4211c57fa920bfa44740a6cf1ab
blockyfy <blockyfy@gmail.com>
2023-12-15 15:50:56 +0500

    latest code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6baca481898aaa320c5da6500004775fab89a565',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6baca481898aaa320c5da6500004775fab89a565',
+    ),
+  ),
+);
